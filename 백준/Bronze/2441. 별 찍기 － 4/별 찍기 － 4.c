43d70a9e9669d974bf0fd3deb8820ca71b1cc699
jayok7172 <jayok7172@naver.com>
2025-07-08 18:11:08 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = N; i >= 1; i--) {
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
