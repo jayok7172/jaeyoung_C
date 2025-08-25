26ec50e5bf8661e286d644403258603bff2686c3
jayok7172 <jayok7172@naver.com>
2025-08-25 18:59:49 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,17 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 	int N;
 	scanf("%d", &N);
-	for (int i = 2; i; i++) {
+	for (int i = 2; i <= N; i++) {
 		if (N % i == 0) {
-			printf("%d\n", i);
 			N = N / i;
+			printf("%d\n", i);
 			i--;
 		}
-		if (i > N) break;
 	}
-
 }
