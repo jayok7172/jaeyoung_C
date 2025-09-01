37e7714b8562256c65bbe63e26583fe371623b78
jayok7172 <jayok7172@naver.com>
2025-09-02 02:30:51 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int max = 0, min = 1000001;
+	for (int i = 0; i < n; i++) {
+		int a;
+		scanf("%d", &a);
+		if (a > max) max = a;
+		if (a < min) min = a;
+	}
+	printf("%d", max * min);
+}
