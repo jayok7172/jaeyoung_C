13315989315021476f44c16e2be1c7f8d17c4639
jayok7172 <jayok7172@naver.com>
2025-09-02 02:14:28 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,23 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void) {
-    int n, c = 1;
-    long long num = 1;
-
-
-    while (~scanf("%d", &n)) { 
-        num = 1;
-        c = 1;
-        while (1) {
-            if (num % n != 0) {
-                num = (num % n) * 10 + 1; 
-                c++;
-            }
-            else {
-                printf("%d\n", c);
-                break;
-            }
-        }
-    }
-    return 0;
+int main() {
+	int n;
+	while (scanf("%d", &n)!=EOF) {
+		int cnt = 1, x = 1;
+		while (1) {
+			if (x % n == 0) {
+				printf("%d\n", cnt);
+				break;
+			}
+			else {
+				x = (x % n) * 10 + 1;
+				cnt++;
+			}
+		}
+	}
 }
