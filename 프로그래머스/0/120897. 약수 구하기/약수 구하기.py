6f3f29109597dd8e27e3544cbe119a73e172aa30
jayok7172 <jayok7172@naver.com>
2026-03-30 19:06:28 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            answer.append(i)
+            if i != n // i:  # 짝 약수
+                answer.append(n // i)
+    
+    return sorted(answer)
