86bb63f687bb480d71f7da30829ba90943ed383e
jayok7172 <jayok7172@naver.com>
2026-04-08 21:00:58 +0900

    [level 1] Title: 소수 만들기, Time: 17.40 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for i in combinations(nums,3):
+        s= sum(i)
+        a = True
+        for j in range(2,int(s**0.5)+1):
+            if s % j ==0:
+                a = False
+                break
+        if a == True:
+            answer +=1
+    return answer
