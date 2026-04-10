dff489e70cb5179affecd2645f34c2a329800cae
jayok7172 <jayok7172@naver.com>
2026-04-11 02:52:55 +0900

    [Gold V] Title: 회의실 배정, Time: 228 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -4,15 +4,13 @@ input = sys.stdin.readline
 N = int(input())
 meeting = []
 for i in range(N):
-    x,y = map(int, input().split())
+    x,y = map(int,input().split())
     meeting.append((x,y))
-meeting.sort(key =lambda x:(x[1],x[0]))
-
-room = 1
+meeting.sort(key = lambda x : (x[1],x[0]))
 check = meeting[0][1]
-for i in range(1,N):
-    if meeting[i][0]>=check:
+result =1
+for i in range(1,len(meeting)):
+    if check <= meeting[i][0]:
         check = meeting[i][1]
-        room+=1
-print(room)        
-
+        result +=1
+print(result)
