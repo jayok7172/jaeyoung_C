78776a8d58299aa645a4519225fe6acf6a7967fd
jayok7172 <jayok7172@naver.com>
2026-04-23 20:06:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.22 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nBnrfo9senf6b1y0ObYwUqjA&gclid=CjwKCAjwhqfPBhBWEiwAZo196vwU6_IP7EYtkf9ZTlOZcMi2RSfBeuaLX6NB0qKKUmzy1x1sszNPoBoC250QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 9.47 MB, 시간: 9.94 ms
+메모리: 9.36 MB, 시간: 10.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 11일 13:07:29
+2026년 04월 23일 20:06:05
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,23 @@
 from collections import deque
-
 def solution(maps):
     answer = 0
     queue = deque()
-    n=len(maps)
-    m = len(maps[0])
-    visited = [[False] * m for _ in range(n)]
-    dist = [[0] * m for _ in range(n)]
-    dist[0][0] = 1
-    visited[0][0] = True
-    directions = [(-1,0),(1,0),(0,1),(0,-1)]
     queue.append((0,0))
-    while queue:
-        r,c = queue.popleft()
-        for dr,dc in directions:
-            nr,nc = r+dr, c+dc
-            if 0 <= nr < n and 0<= nc <m:
-                if maps[nr][nc] ==1 and not visited[nr][nc]:
-                    dist[nr][nc] = dist[r][c] +1
-                    visited[nr][nc] = True
-                    queue.append((nr,nc))
-    if dist[n-1][m-1]==0:
+    y,x = len(maps),len(maps[0])
+    directions = [(-1,0),(1,0),(0,-1),(0,1)]
+    visited = [[False]*x for _ in range(y)]
+    dir = [[0]*x for _ in range(y)]
+    dir[0][0] = 1
+    visited[0][0] = True
+    while queue :
+        m,n = queue.popleft()
+        for dm,dn in directions:
+            nm,nn = m + dm, n + dn
+            if 0<=nm<y and 0<=nn<x and visited[nm][nn] ==False and maps[nm][nn]==1:
+                visited[nm][nn] = True
+                dir[nm][nn] = dir[m][n] +1
+                queue.append((nm,nn))
+    if dir[y-1][x-1] == 0:
         return -1
-    else:
-        return dist[n-1][m-1]
+    else :
+        return dir[y-1][x-1]
